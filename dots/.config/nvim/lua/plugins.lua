b0015d0f7cf11d40001fe4fb1f2492fb6ee7bebc
Chaeng <frogdoctor@tutanota.com>
2026-05-06 10:58:50 +0800

    edit vimrc to minimal

--- dots/.config/nvim/lua/plugins.lua
+++ dots/.config/nvim/lua/plugins.lua
@@ -5,13 +5,13 @@ vim.pack.add({
   { src = 'https://github.com/saghen/blink.cmp' },
   { src = 'https://github.com/j-hui/fidget.nvim' },
   { src = 'https://github.com/williamboman/mason.nvim' },
-  { src = 'https://github.com/gbprod/nord.nvim' },
   { src = 'https://github.com/tpope/vim-fugitive' },
   { src = 'https://github.com/rafamadriz/friendly-snippets' },
   { src = 'https://github.com/rebelot/kanagawa.nvim' },
+  -- { src = 'https://github.com/gbprod/nord.nvim' },
   -- { src = 'https://github.com/folke/tokyonight.nvim' },
   -- { src = 'https://github.com/oahlen/iceberg.nvim' },
-  -- { src = 'https://github.com/vague-theme/vague.nvim' },
+  { src = 'https://github.com/vague-theme/vague.nvim' },
 })
 
 require('mini.pick').setup({})
